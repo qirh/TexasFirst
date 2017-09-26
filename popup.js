37148056009e3cb23430aff225f0d33430565f80
saleh alghusson <salehg@live.com>
2017-09-26 10:24:22 -0500

    i'm giving up on doing it this way, going to go with no pop uo

--- popup.js
+++ popup.js
@@ -1,57 +1,31 @@
-
 function getCurrentTabUrl(callback) {
-  console.log("getCurrentTabUrl");
   var queryInfo = {
     active: true,
     currentWindow: true
   };
-
   chrome.tabs.query(queryInfo, (tabs) => {
-
     var tab = tabs[0];
-
     var url = tab.url;
-
     console.assert(typeof url == 'string', 'tab.url should be a string');
     callback(url);
   });
 }
 
-function changeBackgroundColor(color) {
-  console.log("changeBackgroundColor");
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
-
-function getSavedBackgroundColor(url, callback) {
-  console.log("getSavedBackgroundColor");
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-function saveBackgroundColor(url, isOn) {
-  console.log("saveBackgroundColor");
-  var items = {};
-  items[url] = isOn;
-  chrome.storage.sync.set(items);
-}
-$(document).ready(function() {
-    $('#texasCheckID').bootstrapSwitch();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("addEventListener");
-  getCurrentTabUrl((url) => {
-    var switchElem = document.getElementById('texasCheckID');
-
-    console.log("getCurrentTabUrl, url = " + url);
-
-    switchElem.addEventListener('change', () => {
-      //changeBackgroundColor(switchElem.);
-      //saveBackgroundColor(url, drop);
+function clicked() {
+  console.log("2");
+  chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript({
+      code: 'console.log("3");var optionArray = document.getElementsByTagName("option");console.log(optionArray);'
     });
+  });
+}
+
+
+$(document).ready(function() {
+  $('#texasCheckID').bootstrapSwitch();
+
+  $('#texasCheckID').on('switchChange.bootstrapSwitch', function(event, state) {
+    console.log("1");
+    clicked();
   });
 });
