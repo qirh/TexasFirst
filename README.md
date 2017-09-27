af4cdd395190a26dbf7ccc62d40f847ee15e0c8b
saleh alghusson <salehg@live.com>
2017-09-27 15:17:53 -0500

    it works

--- README.md
+++ README.md
@@ -17,11 +17,18 @@ Texas First! Chrome extension
   * Armadillo icon (couldn't fins Texas map icon hehe) from here:
     <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
+  * Inspiration came from here:
+    https://github.com/vpicone/SorryTennesee
+
+  * As always stackoverflow has been a great help, specifically these answers:
+    https://stackoverflow.com/questions/14051739/how-to-change-the-select-option-order
+    https://stackoverflow.com/questions/18113495/javascript-get-list-of-all-values-in-select-box
+    https://stackoverflow.com/questions/16379789/add-new-option-to-all-dropdown-select-tags-using-javascript
+
 ## Author's notes
 
 ### Does not work properly
-  * everything lol
+  * nada hopefully ^_^
 ### Features to implement
-  * icon color switcher
-  * detect when switcher is pressed
-  * detect if selector exists in page
+  * Add options page:
+    * icon color switcher
--- TexasFirst.js
+++ TexasFirst.js
@@ -1,31 +1,39 @@
-console.log("1");
-var optionElems = document.getElementsByTagName("option");
-var optionArray = Array.from(optionElems);
-/*
-var count = 0;
-for (let i = 0; i < optionArray.length; i++) {
-  if (optionArray[i].text.toUpperCase() === "TX" || optionArray[i].text.toUpperCase() === "TEXAS") {
-    var text = optionArray[i-count].text;
-    optionElems[i-count].remove();
-    optionElems.prepend('<option value="i-came-first">Hey!</option>');
-    console.log((i-count) + " -- " + text);
-    console.log(optionElems);
-    count++;
-    console.log(optionElems[0].innerHTML);
+
+//get all selects in the page
+var select_elems = document.getElementsByTagName("select");
+
+if(select_elems.length > 30) {
+  //do nothing if there are more than 30 selects, safeguard so it doesn't loop 4evah
+} else {
+  //loop over selects
+  for (var i = 0; i < select_elems.length; i++) {
+
+    //options per select
+    var option_array = Array.from(select_elems[i].options);
+
+    if(option_array.length > 300) {
+      //do nothing if there are more than 300 options, safeguard so it doesn't loop 4evah
+    } else {
+      //loop over options
+      for (var j = 0; j < option_array.length; j++) {
+
+        //if there exists a mention of Tejas
+        if(option_array[j].text.toUpperCase() === "TX" || option_array[j].text.toUpperCase() === "TEXAS"){
+
+          //remove it
+          var text_removed = option_array[j].text;
+          select_elems[i].options[j].remove();
+
+          //create new option
+          var option_elem = document.createElement("option"); //not sure if text and value should be the same
+          option_elem.text = text_removed;
+          option_elem.value = text_removed;
+
+          //add it to the top
+          select_elems[i].insertBefore(option_elem, select_elems[i].options[0])
+          select_elems[i].selectedIndex = 0;
+        }
+      }
+    }
   }
 }
-*/
-
-        var sel = document.getElementsByTagName("select");
-
-        for (var x = 0; x < sel.length; x++) {
-
-           var opt = document.createElement("option");
-           opt.text = 'bla...';
-           opt.value = 'Please Select';
-
-           //prepend in select box
-           sel[x].insertBefore(opt, sel[x].options[0])
-           sel[x].selectedIndex = 0;
-
-        }
