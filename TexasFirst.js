98e131ca68052c7bb048ca2f621c064cbc1cd4bc
saleh alghusson <salehg@live.com>
2017-10-14 13:31:03 -0500

    everything seems to work fine, needs a bit more testing and maybe? a  popup.htmlgit add .

--- TexasFirst.js
+++ TexasFirst.js
@@ -1,37 +1,58 @@
+console.log("1");
 
-//get all selects in the page
-var select_elems = document.getElementsByTagName("select");
+getOption(runScript);
 
-if(select_elems.length > 30) {
-  //do nothing if there are more than 30 selects, safeguard so it doesn't loop 4evah
-} else {
-  //loop over selects
-  for (var i = 0; i < select_elems.length; i++) {
+function getOption(callback) {
+  var defaultState = "true";
+	var state;
+  chrome.storage.sync.get('isOn', function (obj) {
+        if(obj.isOn === undefined)
+          state = defaultState
+        else
+          state = obj.isOn;
+        callback(state);
+  });
+}
 
-    //options per select
-    var option_array = Array.from(select_elems[i].options);
+function runScript(state) {
+	if (state) {
+    //get all selects in the page
+    var select_elems = document.getElementsByTagName("select");
 
-    if(option_array.length > 300) {
-      //do nothing if there are more than 300 options, safeguard so it doesn't loop 4evah
-    } else {
-      //loop over options
-      for (var j = 0; j < option_array.length; j++) {
+    if(select_elems.length > 30) {
+      //do nothing if there are more than 30 selects, safeguard so it doesn't loop 4evah
+    }
+    else {
+      //loop over selects
+      for (var i = 0; i < select_elems.length; i++) {
 
-        //if there exists a mention of Tejas
-        if(option_array[j].text.toUpperCase() === "TX" || option_array[j].text.toUpperCase() === "TEXAS"){
+        //options per select
+        var option_array = Array.from(select_elems[i].options);
 
-          //remove it
-          var text_removed = option_array[j].text;
-          select_elems[i].options[j].remove();
-          
-          //create new option
-          var option_elem = document.createElement("option"); //not sure if text and value should be the same
-          option_elem.text = text_removed;
-          option_elem.value = text_removed;
+        if(option_array.length > 300) {
+          //do nothing if there are more than 300 options, safeguard so it doesn't loop 4evah
+        }
+        else {
+          //loop over options
+          for (var j = 0; j < option_array.length; j++) {
 
-          //add it to the top
-          select_elems[i].insertBefore(option_elem, select_elems[i].options[0])
-          select_elems[i].selectedIndex = 0;
+            //if there exists a mention of Tejas
+            if(option_array[j].text.toUpperCase() === "TX" || option_array[j].text.toUpperCase() === "TEXAS"){
+
+              //remove it
+              var text_removed = option_array[j].text;
+              select_elems[i].options[j].remove();
+
+              //create new option
+              var option_elem = document.createElement("option"); //not sure if text and value should be the same
+              option_elem.text = text_removed;
+              option_elem.value = text_removed;
+
+              //add it to the top
+              select_elems[i].insertBefore(option_elem, select_elems[i].options[0])
+              select_elems[i].selectedIndex = 0;
+            }
+          }
         }
       }
     }
--- options.js
+++ options.js
@@ -1,43 +1,35 @@
+console.log("2");
+
 $(document).ready(function() {
+  console.log("4");
   $('#texasCheckID').bootstrapSwitch();
-  $('#texasCheckID').on('switchChange.bootstrapSwitch', function () {
-      loadOption();
+  $('#texasCheckID').on('switchChange.bootstrapSwitch', function(event, state) {
+    console.log("Event Handler");
+    saveOption(state);
   });
   getOption(loadOption);
 });
-
-var defaultState = "True";
+console.log("3");
 
 function getOption(callback) {
+  var defaultState = "true";
 	var state;
-
   chrome.storage.sync.get('isOn', function (obj) {
-        console.log(obj.isOn);
-        state = obj.isOn;
+        if(obj.isOn === undefined)
+          state = defaultState
+        else
+          state = obj.isOn;
         callback(state);
-    });
-
+  });
 }
 function loadOption(state) {
-  console.log("here1 -- " + state);
-	if (state == undefined ) {
-		state = defaultState;
-	}
-  console.log("here2 -- " + state);
-	if (state === "True") {
-    $("#texasCheckName").bootstrapSwitch('state', true);
-  }
-  else {
-    $("#texasCheckName").bootstrapSwitch('state', false);
-  }
+	if (state)
+    $("#texasCheckID").bootstrapSwitch('state', true);
+  else
+    $("#texasCheckID").bootstrapSwitch('state', false);
 }
 
-function saveOptions(state) {
+function saveOption(state) {
   chrome.storage.sync.set({'isOn': state}, function() {
-          // Notify that we saved.
-          message('Settings saved');
   });
-
-}
-function eraseOptions() {
 }
